106015cfecd4a9683947d7535287c19fe85ab7bc
Jean Hugues Robert <jean_hugues_robert@yahoo.com>
2025-11-08 14:08:56 +0100

    Mineur

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,15 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['remark-gfm'],
+  },
+  build: {
+    sourcemap: true,
+    minify: false
+  },
   server: {
     watch: {
-      usePolling: true,  // ESSENTIEL pour Windows
-      interval: 100
+      usePolling: true,
+      interval: 100,
     },
     hmr: {
-      overlay: true
-    }
-  }
-  
+      overlay: true,
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      css: {
+        charset: false,
+      },
+    },
+  },
+});
