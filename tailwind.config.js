ce54ad0bcc1f9c3aa893ae2873c6b0da85d3c657
Jean Hugues Robert <jean_hugues_robert@yahoo.com>
2025-11-26 21:20:28 +0100

    Better tailwind integration, no more cdn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,4 @@
 module.exports = {
-  content: ['./index.html', './src/**/*.{js,jsx}'],
-  theme: {
-    extend: {
-      colors: {
-        primary: {
-          DEFAULT: '#F54928',
-          light: '#FFA726'
-        },
-        secondary: {
-          DEFAULT: '#0A3F73',
-          light: '#42A5F5'
-        },
-        accent: {
-          green: '#66BB6A',
-          orange: '#FF5722',
-          blue: '#1F6FEB'
-        }
-      },
-      fontFamily: {
-        inter: ['Inter', 'sans-serif']
-      }
-    }
-  },
-  plugins: [require('@tailwindcss/typography')]
-}
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  plugins: [require("@tailwindcss/typography")],
+};
