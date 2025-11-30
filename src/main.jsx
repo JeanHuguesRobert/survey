c60599b76b05773febc4ede5768dbd59d10f75d3
Jean Hugues Robert <jean_hugues_robert@yahoo.com>
2025-11-30 11:54:26 +0100

    fix small false security error about Facebook and move Facebook SDK to index.html

--- src/main.jsx
+++ src/main.jsx
@@ -9,38 +9,6 @@ import { GlobalStatusProvider } from "./contexts/GlobalStatusContext";
 import { CurrentUserProvider } from "./contexts/CurrentUserContext";
 import "./styles/index.css"; // Import des styles globaux (New Architecture)
 
-// Conditionally load Facebook JS SDK when a client App ID is provided
-const FB_APP_ID = import.meta.env.VITE_FACEBOOK_APP_ID;
-if (FB_APP_ID) {
-  window.fbAsyncInit = function () {
-    try {
-      FB.init({
-        appId: FB_APP_ID,
-        cookie: true,
-        xfbml: true,
-        version: "v16.0",
-      });
-
-      // Example: check login status right after init (no-op if not used)
-      FB.getLoginStatus(function (response) {
-        // response.status can be: 'connected', 'not_authorized', 'unknown'
-        console.log("FB login status:", response);
-      });
-    } catch (e) {
-      console.error("FB SDK init error", e);
-    }
-  };
-
-  (function (d, s, id) {
-    if (d.getElementById(id)) return;
-    const js = d.createElement(s);
-    js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js";
-    const fjs = d.getElementsByTagName(s)[0];
-    fjs.parentNode.insertBefore(js, fjs);
-  })(document, "script", "facebook-jssdk");
-}
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <SupabaseProvider>
     <CurrentUserProvider>
