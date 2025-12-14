37447b7f228e9e225bf287d8367c95626e807800
Jean Hugues Robert <jean_hugues_robert@yahoo.com>
2025-12-14 23:28:40 +0100

    Pb de build en deploiement, je tente d'imposer NodeJS version 24

--- public/meta-init.js
+++ public/meta-init.js
@@ -1,7 +1,7 @@
 // Dynamic metadata initialization
 // ⚠️ FICHIER GÉNÉRÉ AUTOMATIQUEMENT - NE PAS MODIFIER
 // Généré par scripts/generate-meta-init.js lors du build
-// Dernière génération: 2025-12-14T17:04:10.868Z
+// Dernière génération: 2025-12-14T22:17:23.837Z
 
 const CITY_NAME = "Corte";
 const PARTY_NAME = "Petit Parti";
